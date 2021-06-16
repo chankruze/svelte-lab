c664bfe9e5da455e0d192d4849d24ca21f651095
chankruze <chankruze@gmail.com>
2021-06-16 07:26:20 +0530

    Binding HTML & XSS Sample
    Signed-off-by: chankruze <chankruze@gmail.com>

--- hello-world/src/App.svelte
+++ hello-world/src/App.svelte
@@ -1,9 +1,33 @@
 <script>
   const name = "chandan";
+  const richName = "<b>chandan</b>";
+
+  /* helper: just for demo */
+  function getCookie(c_name) {
+    return localStorage.getItem(c_name);
+  }
+
+  function setCookie(c_name, value, expiredays) {
+    return localStorage.setItem(c_name, value);
+  }
+
+  const fakeUserSessionID = "secret_" + Math.random().toString(36).substring(2);
+  setCookie("sessionID", fakeUserSessionID);
+
+  /* end of helper */
+
+  // malicious users (@dexter) gonna insert this piece of code
+  // into the rich text input field in order to pass their semester
+  const xssHack = `
+  <a href="#" onClick="alert('sessionID: ' + localStorage.getItem('sessionID'))">
+  Click to Claim Your iPhone XR Pro Max Ultra Lite!
+  </a>`;
 </script>
 
 <main>
   <h1>Hello {name}</h1>
+  <h2>{@html richName}</h2>
+  <h2>{@html xssHack}</h2>
 </main>
 
 <style>
